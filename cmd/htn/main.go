89e4f4c0aec75e9e9add2e870673798bddc1a45b
wipdev-tech <wipdev.tech@gmail.com>
2024-05-02 12:32:32 +0300

    Test request

--- cmd/htn/main.go
+++ cmd/htn/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	req, err := http.NewRequest(http.MethodGet, "https://wipdev.netlify.app", nil)
+	if err != nil {
+		return fmt.Errorf("error forming the request: %v", err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending the request: %v", err)
+	}
+
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %v", err)
+	}
+
+	fmt.Println(string(resBody))
+
+	return err
 }
