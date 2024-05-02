acd572e68933d24b7ab7f75588eb365f47b8c803
wipdev-tech <wipdev.tech@gmail.com>
2024-05-02 12:40:20 +0300

    Add Bubble Tea example

--- cmd/htn/main.go
+++ cmd/htn/main.go
@@ -5,9 +5,53 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model int
+type tickMsg time.Time
+
+func (m model) Init() tea.Cmd {
+	return tick()
+}
+
+func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := message.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+		}
+
+	case tickMsg:
+		m--
+		if m <= 0 {
+			return m, tea.Quit
+		}
+		return m, tick()
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("\n\n     Hi. This program will exit in %d seconds...", m)
+}
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
+
 func main() {
+	p := tea.NewProgram(model(5), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+
 	err := run()
 	if err != nil {
 		log.Fatal(err)
